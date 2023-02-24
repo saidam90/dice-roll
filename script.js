5b16a3660815b24fd157b304080fd52bcf77c845
Saida Murtazali <saida.murtazaliyeva@yahoo.com>
2023-02-24 12:57:23 +0400

    added toggling

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 'use strict';
 
 //Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const currentScore0El = document.querySelector('#current--0');
 const currentScore1El = document.querySelector('#current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-// const player0El = document.querySelector('.player--0');
-// const player1El = document.querySelector('.player--1');
 
 let currentScore = 0;
 let activePlayer = 0; //0 or 1
@@ -33,18 +34,12 @@ btnRoll.addEventListener('click', function () {
     document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else {
     //1.set the current score to 0;
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     //2.Set active player from player 0 to player 1 and vice versa
     activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
     //3.css - switch between pink colors to show the active player
-    player0El.classList.remove('player--active');
-    player1El.classList.add('player--active');
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
-
-  // const switchPlayer = function () {
-  //   //3.add score to player 1;
-  //   currentScore += dice;
-  //   currentScore1El.textContent = currentScorePlayer1;
-  // };
 });
